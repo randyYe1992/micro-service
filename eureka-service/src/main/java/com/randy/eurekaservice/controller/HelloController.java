ff518011cf3ae0bf601a09fe821dd98be4ae2b0d
randyYe <yerongbiao@163.com>
2018-08-28 22:37:45 +0800

    建立双节点集群注册中心

--- eureka-service/src/main/java/com/randy/eurekaservice/controller/HelloController.java
+++ eureka-service/src/main/java/com/randy/eurekaservice/controller/HelloController.java
@@ -1,13 +1,23 @@
 package com.randy.eurekaservice.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+    @Autowired
+    private DiscoveryClient client;
 
     @RequestMapping("/sayHello")
     public String sayHello() {
+        ServiceInstance instance = client.getLocalServiceInstance();
+        logger.info("host:"+instance.getHost()+" and serviceId:"+instance.getServiceId());
         return "Hello,eureka-servive";
     }
 }
